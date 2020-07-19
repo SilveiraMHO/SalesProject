ce2f4c67921f2b34efb7ece2137ef1997fb83d15
Mario Silveira <mario.silveira@automindautomacao.onmicrosoft.com>
2020-07-19 17:44:57 -0300

    Other entities and second Migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace SalesWebMVC.Models
@@ -9,5 +10,26 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller sl)
+        {
+            Sellers.Add(sl);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(x => x.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,31 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amout { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Sellers { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amout, SaleStatus status, Seller sellers)
+        {
+            Id = id;
+            Date = date;
+            Amout = amout;
+            Status = status;
+            Sellers = sellers;
+        }
+    }
+}
