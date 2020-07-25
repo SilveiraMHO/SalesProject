e8176b7354005824deaede5eb7509fcc30265a8b
Mario Silveira <mario.silveira@automindautomacao.onmicrosoft.com>
2020-07-25 19:36:08 -0300

    Foreign Key not null (Referential Integrity)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -17,6 +17,7 @@ namespace SalesWebMVC.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
 
         public Seller()
